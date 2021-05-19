89fb1f5798a56936117177b139d13f0fca5bfe2b
BooMajka <visionseers@gmail.com>
2021-05-19 15:51:58 +0200

    cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export const Cart = (props) => {
+    const cartItems = [{id: "c1", name: "Sushi", amount: 2, price: 12.99}]
+    .map(el => <li>{el.name}</li>);
     
-}
+    return (
+        <>
+        <div>
+            <ul className="cart-items">
+           {cartItems}
+           </ul>
+        </div>
+        <div className="total">
+            <span>Todal Amount</span>
+            <span>36.62</span>
+        </div>
+        <div className="actions">
+          <button className="button--alt">Close</button>  
+          <button className="button">Order</button>  
+        </div>
+        </>
+    );
+};
