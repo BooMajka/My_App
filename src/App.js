3c53f3ac421fbd8bd14c50aa6594f6a9d4d5fd5f
BooMajka <visionseers@gmail.com>
2021-05-22 15:55:37 +0200

    cart context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Header} from './components/Layout/Header';
 import {Meals} from './components/Meals/Meals';
+import {CartProvider} from './store/CartProvider';
 
 
 
@@ -9,11 +10,10 @@ export const App = () => {
 
   return (
     
-    <>
+    <CartProvider>
         <Header/>
         <Meals/>
-         
-    </>
+    </CartProvider>
   
   );
 };
--- src/components/Cart/HeaderCartButton.js
+++ src/components/Cart/HeaderCartButton.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {useState} from "react";
+import {useState} from 'react';
+import {useContext} from 'react';
 import {CartIcon} from './CartIcon';
 import {Cart} from './Cart';
+import {CartContext} from '../../store/cart-context';
+
 
 
 export const HeaderCartButton = (props) => {
     const [isModalOpen, setModalOpen] = useState(false);
-  
+    const cartCtx = useContext(CartContext);
+    
+    const numberOdCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    },0);
+    
     return (
         <>
     <button onClick={()=> setModalOpen(true)} className="button">
@@ -15,7 +23,7 @@ export const HeaderCartButton = (props) => {
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className="badge">3</span>
+        <span className="badge">{numberOdCartItems}</span>
     </button>
     {isModalOpen && <div className="back-plate" onClose={setModalOpen}/>}
     {isModalOpen && <Cart onClose={setModalOpen}/>}
