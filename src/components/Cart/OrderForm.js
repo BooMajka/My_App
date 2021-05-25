bd4ba12f5cd9888f8ad9a8f6a091ac7caa50f968
BooMajka <visionseers@gmail.com>
2021-05-25 19:59:58 +0200

    ostatnie poprawki

--- src/components/Cart/OrderForm.js
+++ src/components/Cart/OrderForm.js
@@ -1,8 +1,5 @@
 import React, {useState} from "react";
 import {db} from "../../firebase";
-
-
-
 export const OrderForm = ({cart}) => {
     const [error, setError] = useState([]);
     const [inputs, setInputs] = useState({
@@ -11,54 +8,61 @@ export const OrderForm = ({cart}) => {
         address: "",
         phone: "",
     });
-   
-    
-    const handleInputChange = (e) => {
-        const {value} = e.target;
-        setInputs(value);    
-        };
-        
-        const handleSubmit = e => {
-            e.preventDefault();
-            
-            
-            // const errors = [];
-            
-            // if(inputs.user.length < 2 ) {
-            // errors.push("Name is too short");
-            // } 
-            
-            // if(inputs.surname.length < 2 ) {
-            // errors.push("Surname is too short"); 
-            // }
-            
-            // if(inputs.address.length < 2 ) {
-            // errors.push("Address is too short"); 
-            // }
-            
-            // setError(errors);
-            
-            
-            if(db) {
-                db.collection("Ordered_food").add({
-                id: cart[0].id,
-                name: cart[0].name,
-                amount: cart[0].amount,
-                price: cart[0].price,
+    const handleInputName = (e) => {
+        const value = e.target.value;
+        setInputs({user: value});
+    };
+    const handleInputSurname = (e) => {
+        const value = e.target.value;
+        setInputs(prev => ({
+            ...prev,
+            surname: value}));
+    };
+    const handleInputAddres = (e) => {
+        const value = e.target.value;
+        setInputs(prev => ({
+            ...prev,
+            address: value}));
+    };
+    const handleInputPhone = (e) => {
+        const value = e.target.value;
+        setInputs(prev => ({
+            ...prev,
+            phone: value}));
+    };
+    const handleSubmit = e => {
+        e.preventDefault();
+        // const errors = [];
+        // if (inputs.user.length < 2) {
+        //     errors.push("Name is too short");
+        // }
+        // if (inputs.surname.length < 2) {
+        //     errors.push("Surname is too short");
+        // }
+        // if (inputs.address.length < 2) {
+        //     errors.push("Address is too short");
+        // }
+        // setError(errors);
+        if (db) {
+            db.collection("Ordered_food").add({
+                user: inputs.user,
+                surname: inputs.surname,
+                address: inputs.address,
+                phone: inputs.phone,
+                ...cart
             });
-            }
         };
-        
-        console.log(inputs);
-    
+    };
+    console.log(cart);
+    console.log(inputs);
     return (
         <form className="order-form" onSubmit={handleSubmit}>
-            <input value={inputs.user} name="user" placeholder="name" onChange={handleInputChange} />
-            <input value={inputs.surname} name="surname" placeholder="surname" onChange={handleInputChange} />
-            <input value={inputs.address} name="address" placeholder="address" onChange={handleInputChange} />
-            <input value={inputs.phone} name="phone" placeholder="phone number" onChange={handleInputChange} />
+            <input onChange={handleInputName} value={inputs.user} name="name" placeholder="name" />
+            <input value={inputs.surname} name="surname" placeholder="surname" onChange={handleInputSurname}/>
+            <input value={inputs.address} name="address" placeholder="address" onChange={handleInputAddres}/>
+            <input value={inputs.phone} name="phone" placeholder="phone number" onChange={handleInputPhone}/>
             <button className="button">Submit</button>
-             {error.map(el => <p className="error">{el}</p>)} 
+            {error.map(el => <p className="error">{el}</p>)}
         </form>
-    )
-}
+    );
+};
